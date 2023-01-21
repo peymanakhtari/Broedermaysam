232819b0de8434051ea51d816fc2a5fd0d91578c
peymanakhtari <53125479+peymanakhtari@users.noreply.github.com>
2023-01-21 13:59:57 +0330

    2nd Commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,9 +8,7 @@ namespace Maysam.Data
         DbSet<Review> Reviews { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-     //  optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MaysamDB;Integrated Security=True");
-            optionsBuilder.UseSqlServer("Data Source = 5.9.192.37,2019; Persist Security Info=True; User ID = broederm_; Password = *Tw7@H567ZcStt");
-      //  optionsBuilder.UseSqlServer("Data Source = .; Persist Security Info=True; User ID = broederm_; Password = *Tw7@H567ZcStt");
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MaysamDB;Integrated Security=True");
         }
     }
 }
